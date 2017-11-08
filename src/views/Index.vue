1cf121e431b0dff2182eed1b76504faddf9d0ae4
yinchuandong <johncdyin@gmail.com>
2017-11-09 00:15:41 +1100

    update:

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,45 +2,19 @@
     <div id="index">
         <Navigator/>
         <WorkList />
-    	<section class="recommend">
-    		<h1 class="title">大咖推荐</h1>
-    		<ul class="recommend-list">
-    			<li class="item-recommend">
-    				<b class="avatar-m" style="background-image:url(http://qzonestyle.gtimg.cn/brand/style/img/head/default.png);"></b>
-    				<div class="info-con">
-    					<p class="username">中文名就不酷了中文名就不酷了中文名就不酷了</p>
-    					<p class="major">这是他的专业描述</p>
-    				</div>
-    				<b class="icon-arrow"></b>
-    			</li>
-    			<li class="item-recommend">
-    				<b class="avatar-m" style="background-image:url(http://img.ycg.qq.com/e4084a95-3251-4151-a4ff-6d7e5f06addc/small);"></b>
-    				<div class="info-con">
-    					<p class="username">中文名就不酷了中文名就不酷了中文名就不酷了</p>
-    					<p class="major">这是他的专业描述</p>
-    				</div>
-    				<b class="icon-arrow"></b>
-    			</li>
-    			<li class="item-recommend">
-    				<b class="avatar-m" style="background-image:url(http://img.ycg.qq.com/b5d5e536-18b0-4751-af46-ec1b5cf6506a/small);"></b>
-    				<div class="info-con">
-    					<p class="username">中文名就不酷了中文名就不酷了中文名就不酷了</p>
-    					<p class="major">这是他的专业描述</p>
-    				</div>
-    				<b class="icon-arrow"></b>
-    			</li>
-    		</ul>
-    	</section>
+        <RecommendList />
+
     </div>
 </template>
 
 <script>
 import Navigator from '../components/Navigator.vue'
 import WorkList from '../components/WorkList.vue'
+import RecommendList from '../components/RecommendList.vue'
 
 export default {
   name: 'Index',
-  components: { Navigator, WorkList }
+  components: { Navigator, WorkList, RecommendList }
 }
 </script>
 
--- src/views/Viewer.vue
+++ src/views/Viewer.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="page-viewer">
-  	<button class="icon-close-w"></button>
+  	<button class="icon-close-w" v-on:click="goBack()"></button>
   	<ul class="img-list">
-  		<!-- <li class="img-item"><img src="http://img.ycg.qq.com/ab800f4a-a377-4e54-8334-ca3b18ac9b04/preview" alt=""></li>
-  		<li class="img-item"><img src="http://img.ycg.qq.com/2501ea46-63ef-4e2e-8b16-cd08ece69749/preview" alt=""></li> -->
+  		<!-- <li class="img-item"><img src="http://img.ycg.qq.com/ab800f4a-a377-4e54-8334-ca3b18ac9b04/preview" alt=""></li> -->
+  		<!-- <li class="img-item"><img src="http://img.ycg.qq.com/2501ea46-63ef-4e2e-8b16-cd08ece69749/preview" alt=""></li> -->
   		<!-- poster是视频还没播放是看到的图片 -->
-  	<div class="player">
-      <video poster="/static/videos/oceans.png" controls width="100%">
-        <source src="/static/videos/oceans.mp4" type="video/mp4">
-      </video>
-  	</div>
+  		<li class="img-item" style="height:200px; background: #262826;"></li>
+      <li class="img-item">
+      	<div class="player">
+          <video poster="/static/videos/oceans.png" controls width="100%">
+            <source src="/static/videos/oceans.mp4" type="video/mp4">
+          </video>
+      	</div>
+      </li>
+  		<li class="img-item" style="height:300px; background: #262826;"></li>
+  		<!-- <li class="img-item"><img src="http://img.ycg.qq.com/2501ea46-63ef-4e2e-8b16-cd08ece69749/preview" alt=""></li> -->
   	</ul>
   	<div class="article-detail">
   		<h1 class="title">有用的老人</h1>
@@ -34,6 +39,12 @@ export default {
 
   mounted () {
     // Plyr.setup()
+  },
+
+  methods: {
+    goBack () {
+      this.$router.go(-1)
+    }
   }
 }
 </script>
